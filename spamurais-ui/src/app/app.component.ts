365b68a1b5f119829dee0552df65bdd94d7b64cb
Benoît E. <beneccli@gmail.com>
2024-12-02 12:08:52 +0800

    Add dark theme for global view

--- spamurais-ui/src/app/app.component.ts
+++ spamurais-ui/src/app/app.component.ts
@@ -6,25 +6,25 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   template: `
-    <div class="max-w-lg mx-auto mt-24 bg-white border rounded-lg shadow-lg border-zinc-100">
+    <div class="max-w-lg mx-auto mt-24 bg-white dark:bg-zinc-700 border rounded-lg shadow-lg border-zinc-100 dark:border-zinc-600">
 
       <div class="px-8 py-6 text-lg font-bold">Notifications</div>
 
       <div class="px-8 mb-6">
-        <div class="flex w-full rounded-md bg-zinc-100">
+        <div class="flex w-full rounded-md bg-zinc-100 dark:bg-zinc-800">
           <a class="block w-1/3 m-1 font-light text-center"
               routerLink="/alerts"
-              routerLinkActive="bg-white rounded-md shadow"
+              routerLinkActive="bg-white rounded-md shadow dark:bg-zinc-700"
               ariaCurrentWhenActive="page"
           >Alerts</a>
           <a class="block w-1/3 m-1 font-light text-center"
               routerLink="/important"
-              routerLinkActive="bg-white rounded-md shadow"
+              routerLinkActive="bg-white rounded-md shadow dark:bg-zinc-700"
               ariaCurrentWhenActive="page"
           >Important</a>
           <a class="block w-1/3 m-1 font-light text-center"
               routerLink="/archived"
-              routerLinkActive="bg-white rounded-md shadow"
+              routerLinkActive="bg-white rounded-md shadow dark:bg-zinc-700"
               ariaCurrentWhenActive="page"
           >Archived</a>
         </div>
