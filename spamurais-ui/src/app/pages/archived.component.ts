c44bfa3cac2964810df4d00db15ed550c6aa194b
Benoît E. <beneccli@gmail.com>
2024-12-02 12:30:15 +0800

    Add random notifications for all pages

--- spamurais-ui/src/app/pages/archived.component.ts
+++ spamurais-ui/src/app/pages/archived.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { NotificationComponent } from '../components/notification.component';
 
 @Component({
   selector: 'app-archived',
-  imports: [],
+  imports: [NotificationComponent],
   template: `
-    <h1>Archived notifications</h1>
+    <div class="mx-8 mb-6">
+      <div class="bg-zinc-100 dark:bg-zinc-800 p-2 rounded-lg">
+        <app-notification text="Move for youth starts tomorrow"></app-notification>
+        <app-notification text="After work with the team"></app-notification>
+        <app-notification text="Update your password by end of week"></app-notification>
+        <app-notification text="Report expected by end of month"></app-notification>
+      </div>
+    </div>
   `,
 })
 export class ArchivedComponent {
